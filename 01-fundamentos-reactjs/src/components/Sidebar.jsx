356a1ca1bc0c1edd19c12322a465fe1f42a423e8
luanrem <luanrem@gmail.com>
2023-04-22 14:11:47 -0300

    P01C2 - Finalizando Sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +11,14 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/luanrem.png" />
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>

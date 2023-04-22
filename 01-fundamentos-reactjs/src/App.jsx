85167e36afc007efd9d73486561b898d0b17580e
luanrem <luanrem@gmail.com>
2023-04-22 13:58:15 -0300

    P01C2 - Componente: Header / Componente: Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,23 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post author="Diego Fernandes" content="Bla bla bla bla" />
-      <Post author="Gabriel" content="Um novo post" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Diego Fernandes" content="Bla bla bla bla" />
+          <Post author="Gabriel" content="Um novo post" />
+        </main>
+      </div>
     </div>
   )
 }

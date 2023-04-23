e869c7007a6e555fa9fa3d6c27f63a06e171fc45
luanrem <luanrem@gmail.com>
2023-04-22 21:38:43 -0300

    P01C2 - Componente: Avatar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@
 import { PencilLine } from '@phosphor-icons/react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -11,7 +12,8 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/luanrem.png" />
+        <Avatar src="https://github.com/maykbrito.png" />
+
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>

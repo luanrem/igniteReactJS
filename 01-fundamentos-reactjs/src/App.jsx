4f1196a45750f531827796db0812992cd0374b36
luanrem <luanrem@gmail.com>
2023-04-23 19:39:10 -0300

    P01C3 - Iterando no JSX

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,55 @@ import styles from './App.module.css'
 
 import './global.css'
 
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: ""
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luanrem.png',
+      name: 'Luan Roberto',
+      role: 'Student @ rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋", },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀", },
+      { type: 'link', content: "jane.design/doctorcare", },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋", },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀", },
+      { type: 'link', content: "jane.design/doctorcare", },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋", },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀", },
+      { type: 'link', content: "jane.design/doctorcare", },
+    ],
+    publishedAt: new Date('2022-04-22 20:00:00'),
+  },
+]
+
 export function App() {
 
   return (
@@ -14,8 +63,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Diego Fernandes" content="Bla bla bla bla" />
-          <Post author="Gabriel" content="Um novo post" />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>

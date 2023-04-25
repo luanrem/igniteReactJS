a99722afe9d21774cdd46dc91065e3d40a1013ea
luanrem <luanrem@gmail.com>
2023-04-24 21:41:15 -0300

    P01C3 - Comunicação entre componentes

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable react/prop-types */
 import { ThumbsUp, Trash } from '@phosphor-icons/react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/luanrem.png" />
@@ -15,7 +21,7 @@ export function Comment({ content }) {
               <time title='11 de Maio às 11:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -39,6 +39,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentario ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -85,7 +89,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              onDeleteComment={deleteComment}
+              key={comment}
+              content={comment}
+            />
+          )
         })}
       </div>
     </article>

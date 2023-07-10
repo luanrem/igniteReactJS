91cc0dd01e4ad394f770363c0882517de9b607f3
luanrem <luanrem@gmail.com>
2023-07-10 19:46:30 -0300

    P02C1 - Configuranto temas

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+    </ThemeProvider>
   )
 }
 

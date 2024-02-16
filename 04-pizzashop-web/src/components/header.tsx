513885c5b01f973a4df73c69fe2a6faecb29c40d
luanrem <luanrem@gmail.com>
2024-02-15 21:37:50 -0300

    P04C3 - Perfil no cabeçalho

--- 04-pizzashop-web/src/components/header.tsx
+++ 04-pizzashop-web/src/components/header.tsx
@@ -23,6 +23,7 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
           <AccountMenu />
